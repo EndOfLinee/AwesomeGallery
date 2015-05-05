687760060d28e002150f8ff103bebfe2c25ef562
Aleksandar Tanevski <fnk436813@outlook.com>
2015-05-05 02:11:04 +0200

    Fixed imagelist and listview WORKING!!!

--- AwesomeGallery/MainView.cs
+++ AwesomeGallery/MainView.cs
@@ -14,17 +14,18 @@ namespace AwesomeGallery
     public partial class MainView : Form
     {
         private ImageList selectedImages;
+        private ImageList selectedImages1;
         private List<byte[]> pictures;
-        protected Graphics myGraphics;
 
         public MainView()
         {
             InitializeComponent();
             selectedImages = new ImageList();
+            selectedImages1 = new ImageList();
+            
             pictures = new List<byte[]>();
-            myGraphics = Graphics.FromHwnd(panel1.Handle);
         }
-
+        public String[] files;
 
 
         //Menu Open
@@ -32,33 +33,64 @@ namespace AwesomeGallery
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
             openFileDialog1.Multiselect = true;
-
             openFileDialog1.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+
             if (openFileDialog1.ShowDialog().ToString().Equals("OK"))
             {
-                String[] files = openFileDialog1.FileNames;
-                for (int i = 0; i < files.Length; i++)
+                files = openFileDialog1.FileNames;
+               
+                if (files.Count() != 0)
                 {
-                    pictures.Add(File.ReadAllBytes(files[0]));
-                    using (var ms = new MemoryStream(pictures[i]))
+                    for (int i = 0; i < files.Count(); i++)
                     {
-                        selectedImages.Images.Add(openFileDialog1.SafeFileNames[i], Image.FromStream(ms));
+                        
+                        ListViewItem item = new ListViewItem();
+                        item.ImageIndex = i;
+                        item.Text = Path.GetFileNameWithoutExtension(files[i]);
+
+                        selectedImages.Images.Add(Image.FromFile(files[i]));
+                        listView1.BeginUpdate();
+                        listView1.Items.Add(item); 
+                        listView1.EndUpdate();
                     }
+                    this.listView1.View = View.LargeIcon;
+                    this.selectedImages.ImageSize = new Size(50, 50);
+                    this.listView1.LargeImageList = this.selectedImages;
+
+                    pictureBox1.Image = Image.FromFile(files[0]);
+                    
+                    
+                    
                 }
-                using (var ms = new MemoryStream(pictures.ElementAt(0)))
-                {
-                    pictureBox1.Image = Image.FromStream(ms);
-                }
-                selectedImages.ImageSize = new Size(100, 100);
-                listView1.LargeImageList = selectedImages;
-                List<string> names = new List<string>() { "1", "2" };
-                int count = 0;
-                foreach (string s in names)
-                {
-                    ListViewItem lst = new ListViewItem();
-                    lst.ImageIndex = count++;
-                    listView1.Items.Add(lst);
-                }
+
+
+
+
+
+                //for (int i = 0; i < files.Length; i++)
+                //{
+                //    pictures.Add(File.ReadAllBytes(files[0]));
+                //    using (var ms = new MemoryStream(pictures[i]))
+                //    {
+                //        selectedImages.Images.Add(openFileDialog1.SafeFileNames[i], Image.FromStream(ms));
+                //    }
+                //}
+
+                //using (var ms = new MemoryStream(pictures.ElementAt(0)))
+                //{
+                //    pictureBox1.Image = Image.FromStream(ms);
+                //}
+
+                //selectedImages.ImageSize = new Size(50, 50);
+                //listView1.LargeImageList = selectedImages;
+                //List<string> names = new List<string>() { "1", "2" };
+                //int count = 0;
+                //foreach (string s in names)
+                //{
+                //    ListViewItem lst = new ListViewItem();
+                //    lst.ImageIndex = count++;
+                //    listView1.Items.Add(lst);
+                //}
             }
         }
 
@@ -72,6 +104,20 @@ namespace AwesomeGallery
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+            
+            int a = 0;
+            try
+            {
+                a = listView1.SelectedIndices[0];
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                pictureBox1.Image = Image.FromFile(files[a]);
+            }
         }
 
         private void MainView_Load(object sender, EventArgs e)
